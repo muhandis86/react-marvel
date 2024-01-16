d1e868732853677b39c38b204d9904dc673cc32e
Zaur <eskendarov@gmail.com>
2024-01-16 21:00:46 +0300

    marvel - my second app in react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/App';
+import './style/style.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
