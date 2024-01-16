40a169bc07accd253fb4b9eee72ecac8f646ccd9
Zaur <eskendarov@gmail.com>
2024-01-16 21:56:54 +0300

    getResourse from server by API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
+import MarvelService from './services/MarvelService';
+
 import './style/style.scss';
+
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
+// marvelService.getCharacter(1009241).then(res => console.log(res));
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
