4a2c95532c1c47b46e47ac53ef95137743ee0af2
Zaur <eskendarov@gmail.com>
2024-01-20 00:03:15 +0300

    show correct description (my home work)

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -28,6 +28,8 @@ class RandomChar extends Component {
 
     render() {
         const { char: { name, description, thumbnail, homepage, wiki } } = this.state;
+        const validDescr = description !== '' ? description : 'description not found';
+
         return (
             <div className="randomchar">
                 <div className="randomchar__block">
@@ -35,7 +37,7 @@ class RandomChar extends Component {
                     <div className="randomchar__info">
                         <p className="randomchar__name">{name}</p>
                         <p className="randomchar__descr">
-                            {description}
+                            {validDescr}
                         </p>
                         <div className="randomchar__btns">
                             <a href={homepage} className="button button__main">
