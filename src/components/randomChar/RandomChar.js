6f4deb43aff6a5746cc2559a8877bb096f24d0d9
Zaur <eskendarov@gmail.com>
2024-01-23 21:34:36 +0300

    load data in CharInfo

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -17,17 +17,18 @@ class RandomChar extends Component {
 
     componentDidMount() {
         this.updateChar();
-        // this.timerId = setInterval(this.updateChar, 3000);
-    }
-
-    componentWillUnmount() {
-        // clearInterval(this.timerId);
     }
 
     onCharLoaded = (char) => {
         this.setState({
             char,
             loading: false
+        })
+    }
+
+    onCharLoading = () => {
+        this.setState({
+            loading: true
         })
     }
 
@@ -40,6 +41,7 @@ class RandomChar extends Component {
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading();
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -26,10 +26,11 @@ class MarvelService {
         return {
             id: char.id,
             name: char.name,
-            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            description: char.description ? char.description : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
